e7a529efe374641bbe731fab1144a5b98e898fd3
Patrick Cuauro <pcuauronava@my365.bellevue.edu>
2024-03-20 16:49:23 -0400

    fixed footer added flex-layout working on styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,10 +10,16 @@ import { DiagramsComponent } from './diagrams/diagrams.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { NavSidebarComponent } from './nav-sidebar/nav-sidebar.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatMenuModule } from '@angular/material/menu';
+
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FooterComponent } from './footer/footer.component';
 import { MatExpansionModule } from '@angular/material/expansion';
@@ -41,6 +47,11 @@ import { FirebaseTestComponent } from './firebase-test/firebase-test.component';
     MatSidenavModule,
     MatIconModule,
     MatListModule,
+    MatCardModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatMenuModule,
     BrowserAnimationsModule,
     MatExpansionModule
   ],
